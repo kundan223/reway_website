1a39077a5f1aada6b63be54ac6ed2d325919f795
kundan223 <135807600+kundan223@users.noreply.github.com>
2023-10-07 12:23:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,13 @@ ScrollTrigger.refresh();
 
 }
 loco()
+
+document.getElementById('next').onclick = function(){
+  let lists = document.querySelectorAll('.item');
+  document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+  let lists = document.querySelectorAll('.item');
+  document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+
